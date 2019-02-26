892279f9e1117b2196c5b5563640a9bd4b4a25ea
Paige Ruppel <paige@dynamit.com>
2019-02-26 11:52:49 -0500

    refactored into a class based component - POC for initializing and updating state

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return <div>Season Display Div</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+// Specially named function specific to JS - not required by React
+	constructor(props) {
+		super(props);
+		// initialize your state object
+		this.state = { lat: null };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+		window.navigator.geolocation.getCurrentPosition(
+	// success callback
+			(position) => {
+				// must call setState - you CAN NOT mutate state directly
+				this.setState({ lat: position.coords.latitude });
+			},
+	// failure callback
+			(err) => console.log(err)
+		);
+	}
+
+// React requires us to define render
+	render() {
+		return <div>Latitude: {this.state.lat}</div>;
+	}
+}
+
+ReactDOM.render(<App />,document.querySelector('#root'));
