8ef8ef9ee512bbff6db9786bf5b04d5a6105a72b
Paige Ruppel <paige@dynamit.com>
2019-02-26 12:44:21 -0500

    refactored data load to occur in componentDidMount and conditiional display of text

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		// initialize your state object
-		this.state = { lat: null };
+		this.state = { lat: null, errorMessage: '' };
+	}
 
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
 	// success callback
 			(position) => {
@@ -15,13 +17,23 @@ class App extends React.Component {
 				this.setState({ lat: position.coords.latitude });
 			},
 	// failure callback
-			(err) => console.log(err)
+			(err) => {
+				console.error(err);
+				this.setState({ errorMessage: err.message });
+			}
 		);
 	}
 
 // React requires us to define render
 	render() {
-		return <div>Latitude: {this.state.lat}</div>;
+			if (this.state.errorMessage && !this.state.lat) {
+				return <div>{this.state.errorMessage}</div>
+			}
+			if (this.state.lat && !this.state.errorMessage) {
+				return <div>Latitude: {this.state.lat}</div>
+			}
+
+			return <div>Loading...</div>
 	}
 }
 
