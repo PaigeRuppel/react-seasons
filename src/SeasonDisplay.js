f366ae8ef65242348751aab0a38e16c998fb0555
Paige Ruppel <paige@dynamit.com>
2019-02-27 16:08:23 -0500

    finished styling and refactored logic

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,42 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display Div</div>;
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach',
+    icon: 'sun'
+  },
+  winter: {
+    text: 'Brrr it is chilly',
+    icon: 'snowflake'
+  }
+}
+const isSummerMonth = (month) => {
+  return month > 2 && month < 9;
+};
+
+const isNorthernHemisphere = (lat) => {
+  return lat > 0;
+}
+
+const season = (lat, month) => {
+  if (isSummerMonth(month)) {
+    return isNorthernHemisphere(lat) ? 'summer' : 'winter';
+  }
+  return isNorthernHemisphere(lat) ? 'winter' : 'summer';
+}
+
+const SeasonDisplay = (props) => {
+  const seasonName = season(props.lat, new Date().getMonth());
+  const { text, icon } = seasonConfig[seasonName];
+
+  return (
+    <div className={`season-display ${seasonName}`}>
+      <i className={`icon-left massive ${icon} icon`} />
+        <h1>{text}</h1>
+      <i className={`icon-right massive ${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-// Specially named function specific to JS - not required by React
-	constructor(props) {
-		super(props);
-		// initialize your state object
-		this.state = { lat: null, errorMessage: '' };
-	}
+	state = { lat: null, errorMessage: '' };
 
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-	// success callback
-			(position) => {
-				// must call setState - you CAN NOT mutate state directly
-				this.setState({ lat: position.coords.latitude });
-			},
-	// failure callback
-			(err) => {
-				console.error(err);
-				this.setState({ errorMessage: err.message });
-			}
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
 		);
 	}
 
-// React requires us to define render
 	render() {
 			if (this.state.errorMessage && !this.state.lat) {
 				return <div>{this.state.errorMessage}</div>
 			}
 			if (this.state.lat && !this.state.errorMessage) {
-				return <div>Latitude: {this.state.lat}</div>
+				return <SeasonDisplay lat={this.state.lat}/>
 			}
 
 			return <div>Loading...</div>
